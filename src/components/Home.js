910b0ff347649259df11ec8a74d8f3bda1cfcb62
marwa <mds08@mail.aub.edu>
2022-09-14 22:50:45 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name, city } from "../data/data";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+      // update the JSX being returned!
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
